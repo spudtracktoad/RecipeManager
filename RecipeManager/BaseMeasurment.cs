f1e14de94b174a5367735abea04baf5455598739
spudtracktoad <spudtracktoad@gmail.com>
2017-02-24 21:35:47 -0500

    initial commit

---
+++ RecipeManager/BaseMeasurment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Shared
+{
+    public class BaseMeasurment
+    {
+        #region " Private "
+        #endregion
+        #region " Public "
+        #endregion
+
+    }
+}
